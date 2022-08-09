8ced17df2b3e12f1827e11a53b94f44d712607cc
unniiiverse <unniiiverse@gmail.com>
2022-08-09 14:08:32 +0300

    cart script updated and finished

--- js/app.js
+++ js/app.js
@@ -1,83 +1,123 @@
 // Set id
-let items = [];
+let cartWrapper = document.querySelector('.products-cart_wrapper');
 document.querySelectorAll('.products-card').forEach((el, i) => el.setAttribute('data-card-id', i));
 
 window.addEventListener('click', (e) => {
     const self = e.target;
     let cardParent = self.closest('.products-card');
+    let cartParent = self.closest('.products-cart');
+    let items = [];
 
     if (cardParent) {
         // Stepper
         let stepper = cardParent.querySelector('.products-card_counter');
-        let stepperValue = stepper.querySelector('.products-card_value');
+        let sRemove = stepper.querySelector('.products-card_remove');
+        let sValue = stepper.querySelector('.products-card_value');
+        let sAdd = stepper.querySelector('.products-card_add');
 
-        if (self === stepper.querySelector('.products-card_add')) {
-            stepperValue.innerText++;
-        } else if (self === stepper.querySelector('.products-card_remove')) {
-            if (stepperValue.innerText != 1) {
-                stepperValue.innerText--;
+        if (self === sRemove) {
+            if (sValue.innerText != 1) {
+                sValue.innerText--;
             } else {
-                console.warn('Нельзя выбрать меньше 1го товара!')
+                console.warn('Нельзя выбрать меньше 1го товара!');
             }
-        }
-
-        // Get price
-        let price;
-        let sale;
-
-        if (!cardParent.classList.contains('products-card--sale')) {
-            price = cardParent.querySelector('.products-card_cost').innerText;
-            sale = false;
-        } else {
-            price = cardParent.querySelector('.products-card_sale').innerText;
-            sale = true;
+        } else if (self === sAdd) {
+            sValue.innerText++;
         }
 
         // Add to cart
         let toCart = cardParent.querySelector('.products-card_addToCart');
 
         if (self === toCart) {
-            let id = +cardParent.getAttribute('data-card-id');
+            let price;
+            let sale;
 
-            let data = {
-                id,
-                title: cardParent.querySelector('.text-heading_2').innerText,
-                img: cardParent.querySelector('img').getAttribute('src'),
-                price: +price,
-                isSale: sale,
-                count: +stepperValue.innerText,
+            if (!cardParent.classList.contains('products-card--sale')) {
+                price = +cardParent.querySelector('.products-card_cost').innerText;
+                sale = false;
+            } else {
+                price = +cardParent.querySelector('.products-card_sale').innerText;
+                sale = true;
             }
-            
-            items.push(data);
-            stepperValue.innerText = 1;
+
+            let itemData = {
+                id: +cardParent.getAttribute('data-card-id'),
+                title: cardParent.querySelector('.text-heading_2').innerText,
+                price,
+                sale,
+                count: +sValue.innerText,
+            }
+
+            items.push(itemData);
+
+            addItemToCart(itemData);
+            updateRemoveItemBtns();
+            checkCartState();
         }
 
-        // Get total
-        function getTotal() {
-            let totalPrice = [];
-            let total;
+        function addItemToCart(itemData) {
+            const template = `
+                <li class="products-cart_item" data-cartItem-id="${itemData.id}" data-cartItem-getTotal="false">
+                    <button class="products-cart_remove">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20.25 5.25L3.75 5.25001" stroke="#DD4456" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M9.75 9.75V15.75" stroke="#DD4456" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                            <path d="M14.25 9.75V15.75" stroke="#DD4456" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                            <path d="M8.25 2.25H15.75" stroke="#DD4456" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                            <path d="M18.75 5.25V19.5C18.75 19.6989 18.671 19.8897 18.5303 20.0303C18.3897 20.171 18.1989 20.25 18 20.25H6C5.80109 20.25 5.61032 20.171 5.46967 20.0303C5.32902 19.8897 5.25 19.6989 5.25 19.5V5.25" stroke="#DD4456" stroke-width="2" stroke-linecap="round"       stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                    <div class="products-cart_itemDetails">
+                        <span class="text products-cart_title">${itemData.title}</span>
+                        <span class="text">
+                            <span class="text products-cart_count">${itemData.count}</span>
+                            x
+                            <span class="text products-cart_itemCost">${itemData.price}.00</span>
+                        </span>
+                    </div>
+                </li>
+            `;
 
-            items.forEach(el => totalPrice.push(el.price * el.count));
-            total = totalPrice.reduce((prev, curr) => {
-                return prev + curr;
-            }, 0)
-
-            return total;
+            cartWrapper.insertAdjacentHTML('beforeend', template);
+            updateTotal();
         }
-
-        // Set data in local storage
-        if (self === toCart) {
-            localStorage.removeItem('itemsData');
-            localStorage.setItem('itemsData', JSON.stringify(items));
-        }
-
-    }
-
-    // Desktop cart
-
-    let cartParent = self.closest('.products-cart');
-
-    if (cartParent) {
-        console.log('cart!')
     }
 })
+
+function updateTotal() {
+    let total = 0;
+
+    Array.from(cartWrapper.children).forEach(el => {
+        total += +el.querySelector('.products-cart_count').innerText * +el.querySelector('.products-cart_itemCost').innerText;
+    })
+
+    document.querySelector('.products-cart_total').innerText = `${total}.00`;
+}
+
+function updateRemoveItemBtns() {
+    document.querySelectorAll('.products-cart_remove').forEach(el => {
+        el.addEventListener('click', () => {
+            el.closest('.products-cart_item').remove();
+            updateTotal();
+            checkCartState();
+        })
+    })
+}
+
+function checkCartState() {
+    let cart = document.querySelector('.products-cart');
+    if (!cartWrapper.children.length) {
+        cart.classList.remove('products-cart--hasItem');
+        cart.classList.add('products-cart--empty');
+    } else {
+        cart.classList.remove('products-cart--empty');
+        cart.classList.add('products-cart--hasItem');
+    }
+}
+
+checkCartState();
